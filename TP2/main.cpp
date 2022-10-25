576b1e8a08bd3b6927ff768ac1aa9a8f56bd7ef1
Logan Windows <loganlenevez@gmail.com>
2022-10-25 18:49:28 +0200

    WIP Triangle

--- TP2/main.cpp
+++ TP2/main.cpp
@@ -10,7 +10,7 @@ int main()
 	Rectangle r1(20, 30);
 	Rectangle r2(1, 2);
 	Cercle c1(8, { -1, 0 });
-	Triangle t1({4, 4}, {5, 6}, {9, 4});
+	Triangle t1({1, 2}, {1, 2}, {1, 2});
 
 	std::cout << "Perimetre: " << r1.Perimetre() << std::endl;
 	std::cout << "Surface: " << r1.Surface() << std::endl;
@@ -23,5 +23,10 @@ int main()
 	std::cout << c1.EstDansCercle({ 0, 8 }) << std::endl;
 
 	std::cout << t1.Base() << std::endl;
+	std::cout << t1.Surface() << std::endl;
 	std::cout << t1.Hauteur() << std::endl;
+	std::cout << t1.EstIsocele() << std::endl;
+	std::cout << "toto" << std::endl;
+	std::cout << t1.EstRectangle() << std::endl;
+	std::cout << t1.EstEquilateral() << std::endl;
 }
--- TP2/triangle.cpp
+++ TP2/triangle.cpp
@@ -26,22 +26,58 @@ const float Triangle::Base() {
 
 // Calcul de la hauteur avec la formule de Héron
 const float Triangle::Hauteur() {
-	std::cout << "toto" << std::endl;
 	return (2 * Surface() / Base());
 }
 
+// Calcul de la surface avec la formule de Héron
 const float Triangle::Surface() {
 	const float distanceAB = Longueurs(pointA, pointB);
 	const float distanceAC = Longueurs(pointA, pointC);
 	const float distanceBC = Longueurs(pointB, pointC);
-	const float perimetre = (distanceAB + distanceAC + distanceBC);
-	const float d = perimetre / 2;
+	const float d = (distanceAB + distanceAC + distanceBC) / 2;
 
 	const float a = sqrt(d * (d - distanceAB) * (d - distanceAC) * (d - distanceBC));
 
 	return a;
 }
 
+// Calcul la longueur la longueur entre deux points
 const float Triangle::Longueurs(const Point& p1, const Point& p2) {
 	return sqrt(pow(p2.x - p1.x, 2) + pow(p2.y - p1.y, 2));
+}
+
+const bool Triangle::EstIsocele() {
+	const float distanceAB = Longueurs(pointA, pointB);
+	const float distanceAC = Longueurs(pointA, pointC);
+	const float distanceBC = Longueurs(pointB, pointC);
+
+	if (distanceAB == distanceAC || distanceAB == distanceBC
+		|| distanceAC == distanceBC) {
+		return true;
+	}
+	return false;
+}
+
+const bool Triangle::EstRectangle() {
+	const float distanceAB = Longueurs(pointA, pointB);
+	const float distanceAC = Longueurs(pointA, pointC);
+	const float distanceBC = Longueurs(pointB, pointC);
+
+	if (pow(distanceAB, 2) == pow(distanceAC, 2) + pow(distanceBC, 2) 
+		|| pow(distanceAC, 2) == pow(distanceAB, 2) + pow(distanceBC, 2)
+		|| pow(distanceBC, 2) == pow(distanceAB, 2) + pow(distanceAC, 2)) {
+		return true;
+	}
+	return false;
+}
+
+const bool Triangle::EstEquilateral() {
+	const float distanceAB = Longueurs(pointA, pointB);
+	const float distanceAC = Longueurs(pointA, pointC);
+	const float distanceBC = Longueurs(pointB, pointC);
+
+	if (distanceAB == distanceAC && distanceAC == distanceBC) {
+		return true;
+	}
+	return false;
 }
